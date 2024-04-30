d831082fe92656e502efea58eb6da85194ab57f3
Dusk <asutoshpradhan424@gmail.com>
2024-04-30 14:37:59 +0530

    Add functionality to operator buttons Add Arithmetic functionalities:     - Addition     - Subtraction     - Multiplication     - Division     - Modulus Add Clear screen functionality

--- script.js
+++ script.js
@@ -2,25 +2,76 @@ const result = document.querySelector('#result');
 const topR = document.querySelector('#nums');
 const input = document.querySelector('.inputs');
 
+function display(){
+    result.innerHTML = resT;
+    topR.innerHTML = topT;
+}
+
 function clear(){
-    result.innerHTML = '';
-    topR.innerHTML = '';
+    resT = "";
+    topT = "";
+    op = "";
+    num1 = "";
+    num2 = "";
+    display();
 }
 
 let resT = "";
 let topT = "";
+let op = "";
+let num1 = "";
+let num2 = "";
+
+function operate(n1, op, n2){
+    switch (op) {
+        case '+':
+            return n1 + n2;
+        case '-':
+            return n1 - n2;
+        case '*':
+            return n1 * n2;
+        case '/':
+            return n1 / n2;
+        case '%':
+            return n1 % n2;
+        default:
+            return 'error';   
+    }
+}
+
 let butts = [].slice.call(input.children);
 console.log(butts);
 butts.forEach(butt => {
     butt.addEventListener('click', () => {
         if(butt.textContent === 'C' || butt.textContent === 'CE'){
             clear();
+        }
+        else if('+-*/%'.includes(butt.textContent)){
+            op = butt.textContent
+            topT = num1 + " " + op + " ";
             resT = "";
-            topT = "";
+            display();
+        }
+        else if(butt.textContent === '='){
+            topT = topT + num2 + " = ";
+            num1 = parseInt(num1);
+            num2 = parseInt(num2);
+            resT = operate(num1 , op, num2);
+            display();
+            topT = resT;
+            num1 = resT;
+            num2 = "";
+            op = "";
+        }
+        else if(op != ""){
+            num2 = num2 + butt.textContent;
+            resT = resT + butt.textContent;
+            display();
         }
         else{
+            num1 = num1 + butt.textContent;
             resT = resT + butt.textContent;
-            result.innerHTML = resT;
+            display();
         }
     });
 });
